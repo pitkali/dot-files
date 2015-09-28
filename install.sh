6f0e6060c85ecfb12e9cbdd21fd63ffe8796d425
Piotr Kalinowski <pitkali@gmail.com>
2015-09-28 13:04:42 +0200

    Some fixes to install.sh

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ source $confdir/zsh/zshrc
 [[ -d $HOME/bin ]] || mkdir $HOME/bin
 
 # Bootstrapping of basic libraries required to get all the configuration
-ln -sf $confdir/git-config $HOME/.gitconfig
+ln -sf $confdir/git/config $HOME/.gitconfig
 cd $confdir && git submodule init && git submodule update
 
 cd $confdir/lib || exit 1
@@ -18,7 +18,7 @@ sudo easy_install pip
 sudo pip install --upgrade $confdir/lib/dulwich
 
 ln -sf $confdir/emacs.d .emacs.d
-[[ -z "$EMACS" -o ! -x $EMACS ]] && EMACS=emacs
+[[ -z "$EMACS" ]] && EMACS=emacs
 $EMACS --batch -l .emacs.d/init.el --eval '(my-recompile-init)'
 
 ln -sf $confdir/vim .vim
@@ -27,6 +27,7 @@ ln -sf .vimrc .gvimrc
 
 ln -sf $confdir/init.lisp .sbclrc
 ln -sf $confdir/init.lisp .ccl-init.lisp
+mkdir -p $HOME/.config/common-lisp/source-registry.conf.d
 cat > $HOME/.config/common-lisp/source-registry.conf.d/00-asdf3.conf << EOF
 (:directory "$confdir/lib/asdf3/")
 (:directory "$confdir/lib/asdf3/uiop/")
@@ -50,5 +51,5 @@ ln -sf $confdir/mac/com.pitkali.env2launchctl.plist $HOME/Library/LaunchAgents
 sudo cp $confdir/mac/env2launchctl /usr/libexec
 sudo cp $confdir/mac/com.pitkali.launchd.* /Library/LaunchDaemons/
 
-sudo zsh -c "echo /usr/local/bin | cat - /etc/paths > /etc/paths"
+# sudo zsh -c "echo /usr/local/bin | cat - /etc/paths > /etc/paths"
 sudo zsh -c "echo /Applications/Racket/bin > /etc/paths.d/Racket"
