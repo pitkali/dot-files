5241f721dfce29161dc80ddfc015c225caeccae1
Piotr Kalinowski <pitkali@gmail.com>
2018-07-16 21:35:24 +0200

    Clean up more horribly outdated stuff
    I may one day go back to some Clojure programming, even if its startup
    time reaaallly sucks, but drip is no longer a thing, and it now provides
    its own clj command, easily accessible through its installers. No need
    to roll my own then.
    
    Also, the Elixir version of git upstream is annoying because of how io
    works there (no real time upgrades).

--- clojure/profiles.clj
+++
@@ -1,3 +0,0 @@
-{:user {:plugins [;; [cider/cider-nrepl "0.10.0-SNAPSHOT"]
-                  [lein-exec "0.3.6"]]
-        :dependencies [^:replace [org.clojure/tools.nrepl "0.2.11"]]}}
--- install.sh
+++ install.sh
@@ -9,9 +9,6 @@ source $confdir/zsh/zshrc
 ln -sf $confdir/git/config $HOME/.gitconfig
 cd $confdir && git submodule init && git submodule update
 
-cd $confdir/lib || exit 1
-[[ -d guestrepo ]] || hg clone https://bitbucket.org/selinc/guestrepo
-
 cd $HOME
 ln -sf $confdir/hg/hgrc .hgrc
 sudo easy_install pip
@@ -25,26 +22,8 @@ ln -sf $confdir/vim .vim
 ln -sf $confdir/vim/vimrc .vimrc
 ln -sf .vimrc .gvimrc
 
-ln -sf $confdir/init.lisp .sbclrc
-ln -sf $confdir/init.lisp .ccl-init.lisp
-mkdir -p $HOME/.config/common-lisp/source-registry.conf.d
-cat > $HOME/.config/common-lisp/source-registry.conf.d/00-asdf3.conf << EOF
-(:directory "$confdir/lib/asdf3/")
-(:directory "$confdir/lib/asdf3/uiop/")
-EOF
-cat > $HOME/.config/common-lisp/source-registry.conf.d/42-asd-links.conf << EOF
-(:directory "$HOME/.asd-links/")
-EOF
-
-mkdir $HOME/.lein && ln -sf $confdir/clojure/profiles.clj $HOME/.lein
-ln -sf $confdir/clojure/lein-exec{,-p} $HOME/bin
-for target in clj{,-java,-drip}; do
-    ln -sf $confdir/clojure/clj $HOME/bin/$target
-done
-
 ln -sf $confdir/zsh/zshrc .zshrc
 ln -sf $confdir/zsh/vcs_info.py $HOME/bin
-ln -sf $confdir/zsh/vcs-info.rkt $HOME/bin
 
 [[ -d $HOME/Library/LaunchAgents ]] || mkdir -m 0755 $HOME/Library/LaunchAgents
 ln -sf $confdir/mac/com.pitkali.env2launchctl.plist $HOME/Library/LaunchAgents
